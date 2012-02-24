682b7d503d1b2115c7ee4cc41d03bd4e1fa59bd4
smancill <smancill@alumnos.inf.utfsm.cl>
2012-02-24 19:07:20 -0300

    Update installer script

--- install.sh
+++ install.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 
-VIMDIR="${HOME}/.vim"
-VIMRC="${HOME}/.vimrc"
-GVIMRC="${HOME}/.gvimrc"
-
-cd "${VIMDIR}"
+VIMDIR=${HOME}/.vim
+VIMRC=${HOME}/.vimrc
+GVIMRC=${HOME}/.gvimrc
 
 # Create directory for bundles
-mkdir -p bundle
+mkdir -p ${VIMDIR}/bundle
 
 # Install package manager
-if [[ -d bundle/neobundle.vim ]]; then
-    rm -rf bundle/neobundle.vim
+if [[ -d ${VIMDIR}/bundle/neobundle.vim ]]; then
+    rm -rf ${VIMDIR}/bundle/neobundle.vim
 fi
 
-echo "Installing Package Manager..."
-git clone -q git://github.com/smancill/neobundle.vim.git bundle/neobundle.vim
+echo -e "Installing Package Manager..."
+git clone git://github.com/smancill/neobundle.vim.git \
+        ${VIMDIR}/bundle/neobundle.vim
 
 if [[ $? -eq 0 ]]; then
-    echo "Package Manager installed sucessfully"
-    echo "See https://github.com/smancill/neobundle.vim#readme"
+    echo -e "\nPackage Manager installed sucessfully"
+    echo -e "See https://github.com/smancill/neobundle.vim#readme"
 else
-    echo "Could not install package manager"
+    echo -e "\nCould not install package manager"
     exit
 fi
 
 # Create symbolic links
-ln -sf "${VIMDIR}"/vimrc  "${VIMRC}"
-ln -sf "${VIMDIR}"/gvimrc "${GVIMRC}"
+ln -sf ${VIMDIR}/vimrc  ${VIMRC}
+ln -sf ${VIMDIR}/gvimrc ${GVIMRC}
 
 # Install plugins
-echo "\nInstalling plugins..."
+echo -e "\nInstalling plugins..."
 sleep 2
 vim +NeoBundleInstall! +qa!
-echo "Done!"
+echo -e "\nDone!"
