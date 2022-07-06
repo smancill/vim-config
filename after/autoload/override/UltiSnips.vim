smancill/vim-config

commit cd0c5a2e4f0582394f1ceee7a504866b05e0e8dc

Override :UltiSnipsEdit to prefer private snippets
Recent UltiSnips version changed (simplified?) the way snippets are
found, and stopped supporting the g:UltiSnipsSnippetsDir variable,
hardcoding the private snippets directory to ~/.vim/UltiSnips.
But I used that variable to prefer ~/.vim/private/UltiSnips as the
private snippets directory instead of ~/.vim/UltiSnips.

Now :UltiSnipsEdit will always use ~/.vim/UltiSnips/<ft>.snippets,
and :UltiSnipsEdit! will always list ~/.vim/UltiSnips/<ft>.snippets
along with all the other possible runtime matches, ignoring that I
prefer ~/.vim/private/UltiSnips instead of ~/.vim/UltiSnips (which was
configured by setting g:UltiSnipsSnippetsDir).

Worst, I cannot create a new snippets file in ~/.vim/private/UltiSnips
using :UltiSnipsEdit[!] because :UltiSnipsEdit directly creates a new
file in ~/.vim/UltiSnips and :UltiSnipsEdit! also suggests creating
those new files in ~/.vim/UltiSnips, and then only lists existing files
in the other directories in the runtime.

So, the only solution that doesn't force me to fork UltiSnips is to
override the :UltiSnipsEdit command with my own version, and somehow
hack UltiSnips to stop hardcoding ~/.vim/UltiSnips when selecting which
snippets file to edit.

A first attempt was to set b:UltiSnipsSnippetDirectories to a single
absolute path to the private snippets directory before calling
UltiSnips#Edit (and ensuring it is deleted after):

    function! override#UltiSnips#Edit(bang, ...) abort
      if empty(a:bang)
        let b:UltiSnipsSnippetDirectories = [g:UltiSnipsSnippetsDir]
        autocmd BufEnter <buffer> ++once unlet b:UltiSnipsSnippetDirectories
      endif
      call call(function("UltiSnips#Edit"), [a:bang] + a:000)
    endfunction

UltiSnips would then see a single path and it would ignore all other
snippets directories in the runtime.

But while it worked for :UltiSnipsEdit, the bang version
still hardcodes ~/.vim/UltiSnips in the list of potential directories,

after/autoload/override/UltiSnips.vim
@@ -1,0 +1,25 @@
+py3 << endpy3
+import os
+import vim
+
+from UltiSnips import snippet_manager
+from UltiSnips import vim_helper
+
+_vim_config_old_select_snips_file = snippet_manager._select_and_create_file_to_edit
+
+def _vim_config_override_select_snips_file(potentials):
+    if vim_helper.eval("exists('g:UltiSnipsSnippetsDir')") != "0":
+        default_snippets_dir = os.path.join(vim_helper.get_dot_vim(), "UltiSnips")
+        user_snippets_dir = os.path.expanduser(vim_helper.eval("g:UltiSnipsSnippetsDir"))
+        potentials = {
+            snf.replace(default_snippets_dir, user_snippets_dir)
+            for snf in potentials
+        }
+    return _vim_config_old_select_snips_file(potentials)
+
+snippet_manager._select_and_create_file_to_edit = _vim_config_override_select_snips_file
+endpy3
+
+function! override#UltiSnips#Edit(bang, ...) abort
+  call call(function("UltiSnips#Edit"), [a:bang] + a:000)
+endfunction

after/plugin/override.vim
@@ -1,0 +1,2 @@
+command! -bang -nargs=? -complete=customlist,UltiSnips#FileTypeComplete UltiSnipsEdit
+    \ :call override#UltiSnips#Edit(<q-bang>, <q-args>)

vimrc
@@ -97,8 +97,6 @@ let g:UltiSnipsJumpBackwardTrigger = '<S-TAB>'
 let g:UltiSnipsEditSplit = 'context'
 if s:has_private_dir
   let g:UltiSnipsSnippetsDir = '~/.vim/private/UltiSnips'
-else
-  let g:UltiSnipsSnippetsDir = '~/.vim/UltiSnips'
 endif
 
 " vim-addon-local-vimrc                     {{{2

commit 3dface7164fae7ea38e2ec5c30268de7896e75e4

Fix UltiSnipsEdit command (again)
`:UltiSnipsEdit` fails because it cannot find a `.vim` directory. This
happens because UltiSnips now uses `glob` to add snippets directories
from the runtime path, which returns empty for non-existing directories.
Since I don't use `~/.vim/UltiSnips`, it doesn't exist and the command
now fails, and the workaround introduced by cd0c5a2 (Override
:UltiSnipsEdit to prefer private snippets, 2020-08-11) is not enough to
prefer `~/.vim/private/UltiSnips` as the snippets directories for
`:UltiSnipsEdit`.

But the workaround was needed because UltiSnips removed the variable to
set a prefered directory for `:UltiSnipsEdit`. Fortunately, they
recently re-added the variable (although with a different, very long
name), so I can revert the workaround and restore the behavior of
`:UltiSnipsEdit` the way I want it, and everythin works again.

after/autoload/override/UltiSnips.vim
@@ -1,25 +1,0 @@
-py3 << endpy3
-import os
-import vim
-
-from UltiSnips import snippet_manager
-from UltiSnips import vim_helper
-
-_vim_config_old_select_snips_file = snippet_manager._select_and_create_file_to_edit
-
-def _vim_config_override_select_snips_file(potentials):
-    if vim_helper.eval("exists('g:UltiSnipsSnippetsDir')") != "0":
-        default_snippets_dir = os.path.join(vim_helper.get_dot_vim(), "UltiSnips")
-        user_snippets_dir = os.path.expanduser(vim_helper.eval("g:UltiSnipsSnippetsDir"))
-        potentials = {
-            snf.replace(default_snippets_dir, user_snippets_dir)
-            for snf in potentials
-        }
-    return _vim_config_old_select_snips_file(potentials)
-
-snippet_manager._select_and_create_file_to_edit = _vim_config_override_select_snips_file
-endpy3
-
-function! override#UltiSnips#Edit(bang, ...) abort
-  call call(function("UltiSnips#Edit"), [a:bang] + a:000)
-endfunction

after/plugin/override.vim
@@ -1,2 +1,0 @@
-command! -bang -nargs=? -complete=customlist,UltiSnips#FileTypeComplete UltiSnipsEdit
-    \ :call override#UltiSnips#Edit(<q-bang>, <q-args>)

bundle.vim
@@ -160,7 +160,7 @@ let g:UltiSnipsJumpForwardTrigger = '<TAB>'
 let g:UltiSnipsJumpBackwardTrigger = '<S-TAB>'
 let g:UltiSnipsEditSplit = 'context'
 if $VIM_CONFIG_LOAD_PRIVATE_RC == '1' && isdirectory($VIM_CONFIG_HOME . '/private')
-  let g:UltiSnipsSnippetsDir = $VIM_CONFIG_HOME . '/private/UltiSnips'
+  let g:UltiSnipsSnippetStorageDirectoryForUltiSnipsEdit = $VIM_CONFIG_HOME . '/private/UltiSnips'
 endif
 " }}}
 